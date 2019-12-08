100a17296bf02f0f76a202ee55d041d3783b34e6
Fuwn <17520134@gm.uit.edu.vn>
2019-12-08 16:46:34 +0700

    use ListTile for home_main

--- lib/screens/home_screen/widgets/home_main.dart
+++ lib/screens/home_screen/widgets/home_main.dart
@@ -14,47 +14,16 @@ class _HomeMainState extends State<HomeMain> {
       ),
       body: ListView.builder(
         scrollDirection: Axis.vertical,
-        itemCount: 3,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            padding: EdgeInsets.all(10),
-            child: RaisedButton(
-              onPressed: () {
-                // Navigator.push(context, MaterialPageRoute(builder: (context)=>CourseScreen()));
-              },
-              padding: EdgeInsets.all(10),
-              color: Colors.white,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[Text('SE314.K12')],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Phap Luat Dai Cuong',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                      Text(
-                        'Nguyen Minh Hoang',
-                        style: TextStyle(
-                          fontSize: 10,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
+        itemCount: 20,
+        itemBuilder: (BuildContext context, int index) => Card(
+          child: ListTile(
+            leading: Text('SE312.K12'),
+            title: Text('Pháp luật đại cương'),
+            subtitle: Text('Huỳnh Như Ý'),
+            trailing: Icon(Icons.hotel),
+            onTap: () {},
+          ),
+        ),
       ),
     );
   }
