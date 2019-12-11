602edf26b2257839745879e944e6ebaf60082b09
Trung <trunghuynh1999@gmail.com>
2019-12-12 00:05:14 +0700

    fix merge

--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -2,10 +2,16 @@ import 'package:document/screens/login_screen/widgets/login_logo.dart';
 import 'package:document/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   AuthService _auth = AuthService();
 
   TextEditingController emailController = TextEditingController();
+
   TextEditingController passwordController = TextEditingController();
 
   Future login(BuildContext context) async {
