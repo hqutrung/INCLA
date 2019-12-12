79fdaf221eac6629c28958631a1995b5235dd0eb
Trung <trunghuynh1999@gmail.com>
2019-12-12 22:11:00 +0700

    Dialog QR code

--- lib/screens/session_screen/widget_session_screen/QR.dart
+++ lib/screens/session_screen/widget_session_screen/QR.dart
@@ -5,16 +5,47 @@ class CreateQR extends StatefulWidget {
   _CreateQRState createState() => _CreateQRState();
 }
 
+showCreateQRDialog(BuildContext context) async {
+  await showDialog(
+      context: context,
+      builder: (context) {
+        TextEditingController _textFieldController;
+        return AlertDialog(
+          title: Text('Tạo QR code điểm danh'),
+          content: TextField(
+            keyboardType: TextInputType.number,
+            controller: _textFieldController,
+            decoration: InputDecoration(hintText: "Nhập phút"),
+          ),
+          actions: <Widget>[
+             FlatButton(
+              child:  Text('Hủy'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child:  Text('Bắt đầu'),
+              onPressed: () {
+
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      });
+}
+
 class _CreateQRState extends State<CreateQR> {
   @override
   Widget build(BuildContext context) {
-
-    return GestureDetector(
-      child: Container(
-        child: Icon(
-          Icons.center_focus_weak,
-          size: 250.0,
-        ),
+    return Container(
+      child: IconButton(
+        iconSize: 250.0,
+        onPressed: () {
+          showCreateQRDialog(context);
+        },
+        icon: Icon(Icons.center_focus_weak),
       ),
     );
   }
